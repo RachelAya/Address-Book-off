8c409ce76ca9a391beb46f8c4a64390d5f2a235e
RachelAya <rachel.ayateke@gmail.com>
2018-05-08 18:45:41 +0300

    destination form front and back end

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,16 @@
   {
   return this.firstName + " " + this.lastName;
   }
+
+  function Destination(country,city)
+  {
+    this.countryDest=country;
+    this.cityDest= city;
+  }
+  Destination.prototype.finalDest=function ()
+  {
+  return this.countryDest+ " " + this.cityDest;
+  }
   // user interface logic
   $(document).ready(function()
   {
@@ -23,12 +33,22 @@
       $("input#new-first-name").val("");
       $("input#new-last-name").val("");
     });
+  // $(".contact").last().click(function()
+    // {
+    //  $("#show-contact").show();
+    //  $("#show-contact h2").text(newContact.firstName);
+    //  $(".first-name").text(newContact.firstName);
+    //  $(".last-name").text(newContact.lastName);
+    // });
+    $("form#new-dest").submit(function (event) {
+      event.preventDefault();
 
-    $(".contact").last().click(function()
-    {
-     $("#show-contact").show();
-     $("#show-contact h2").text(newContact.firstName);
-     $(".first-name").text(newContact.firstName);
-     $(".last-name").text(newContact.lastName);
+      var inputtedCountry = $("input#dest-one").val();
+      var inputtedCity = $("input#dest-two").val();
+
+      var newDest= new Destination(inputtedCountry,inputtedCity);
+      $("ul#Destinations").append("<li><span class='contact'>"+ newDest.finalDest()+"</span></li>");
+      $("input#dest-one").val("");
+      $("input#dest-two").val("");
     });
   });
